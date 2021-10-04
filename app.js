a1dbc42b0d79986eab5129e8119f4ece62fac2e4
Keith Powell <keith@tapcart.co>
2021-10-03 20:01:09 -0700

    Creating an API

---
+++ app.js
@@ -0,0 +1,110 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const _ = require("lodash")
+
+app = express();
+app.set("view engine", "ejs")
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+//------------------------//
+//       Mongoose         //
+//------------------------//
+
+mongoose.connect("mongodb://localhost:27017/wikiDB")
+
+const articleSchema = new mongoose.Schema({
+    title: "String",
+    content: "String"
+});
+
+const Article = new mongoose.model("Article", articleSchema)
+
+const createArticle = () => {
+
+    Article.create({title: "BitRise", content: "Bitrise is a Continuous Integration and Delivery (CI/CD) Platform as a Service (PaaS) with a main focus on mobile app development (iOS, Android, React Native, Flutter, and so on). It is a collection of tools and services to help you with the development and automation of your software projects."})
+    
+}
+
+// createArticle();
+
+/*
+ * Index - GET - /articles  - Presentational - respond with all articles
+ * New - GET - /articles/new  - Presentational Form - a page with a form to create a new article
+ * Show - GET - /articles/:id  - Presentational - respond with specific article by id
+ * Create - Post - /articles  - Functional - recieve data from new route to create a article
+ * Edit - GET - /articles/:id/edit  - Presentational Form - respond with a form prefilled with article data
+ * Update - PUT - /articles/:id  - Functional - recieve data from edit to update a specific article
+ * Delete - DELETE - /articles/:id  - Functional - Deletes article by id from request
+ */
+
+//------------------------//
+//    URL Chain Routes    //
+//------------------------//
+
+app.route("/articles")
+    .get((req, res) => {
+        Article.find({}, (err, foundArticles) => {
+            if(err) console.log(err);
+            res.send(foundArticles)
+            console.log(foundArticles);
+        });
+    })
+    .post( async (req, res) => {
+        await Article.create({title: _.upperFirst(req.body.title), content: req.body.content}, (err, createdArticle) => {
+            if (err) console.log(err);
+            console.log(`${createdArticle.title} was created in the database`);
+            res.send("Successfully added the new article")
+        });
+    })
+    .delete((req, res) => {
+        Article.deleteMany((err, deletedArticles) => {
+            if(err) console.log(err);
+            console.log(`${deletedArticles} have been deleted`);
+            res.send("Articles have been deleted")
+        });
+    });
+
+//---------------------------------//
+//  Specific Article Chain Routes  //
+//---------------------------------//
+
+app.route("/articles/:articleTitle")
+    .get((req, res) => {
+        Article.findOne({title: req.params.articleTitle}, (err, foundArticle) => {
+            if(foundArticle) {
+                res.send(foundArticle);
+            } else {
+               res.send("No articles matching that title");
+            }
+        })
+    })
+    .put((req, res) => {
+        Article.findOneAndUpdate(
+            {title: req.params.articleTitle}, 
+            {title: req.body.title, content: req.body.content}, 
+            {overwrite: true}, 
+            (err, updatedArticle) => {
+            if(err) console.log(err, "Article has NOT been updated");
+
+            res.send("Article has been updated");
+        })
+    })
+    .patch((req, res) => {
+
+        Article.findOneAndUpdate(
+            {title: req.params.articleTitle}, 
+            {
+                $set:req.body    
+            }, 
+            (err, updatedArticle) => {
+            if(err) console.log(err, "Article has NOT been updated");
+
+            res.redirect("/articles");
+        })
+    })
+
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+})
---
+++ notes.js
@@ -0,0 +1,35 @@
+[
+    {
+        "_id": "6157c88b0ae1e8e8c458da0c",
+        "title": "REST APIs",
+        "content": "REST is short for REpresentational State Transfer.  It's an architectual style for designing APIs."
+    },
+    {
+        "_id": "5c139771d79ac8eac11e754a",
+        "title": "API",
+        "content": "API stands for Application Programming Interface. It is a set of subroutine definitions, communication protocols, and tools for building software. In general terms, it is a set of clearly defined methods of communication among various components. A good API makes it easier to develop a computer program by providing all the building blocks, which are then put together by the programmer."
+    },
+    {
+        "_id": "5c1398aad79ac8eac11e7561",
+        "title": "Bootstrap",
+        "content": "This is a framework developed by Twitter that contains pre-made front-end templates for web design"
+    },
+    {
+        "_id": "6157d23dbe83fff721f7e99c",
+        "title": "BitRise",
+        "content": "Bitrise is a Continuous Integration and Delivery (CI/CD) Platform as a Service (PaaS) with a main focus on mobile app development (iOS, Android, React Native, Flutter, and so on). It is a collection of tools and services to help you with the development and automation of your software projects.",
+        "__v": 0
+    },
+    {
+        "_id": "615a424a40b3cd383146b944",
+        "title": "MongoDB",
+        "content": "MongoDB is a source-available cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with optional schemas. MongoDB is developed by MongoDB Inc. and licensed under the Server Side Public License.",
+        "__v": 0
+    },
+    {
+        "_id": "615a44030ca2467abe659801",
+        "title": "React JS",
+        "content": "React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.",
+        "__v": 0
+    }
+]
